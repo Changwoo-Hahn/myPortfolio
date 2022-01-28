ffbdabf4578e8c276fb70c22c8c489edf9145b29
Chang <hahncwart@gmail.com>
2022-01-28 13:27:42 -0500

    append child

--- dino2/javascript/cloud.js
+++ dino2/javascript/cloud.js
@@ -16,7 +16,7 @@ export function createClouds() {
     cloudElem.classList.add("cloud");
     setCustomProperty(cloudElem, "--top", randomBetween(5, 65));
     //worldElem.appendChild(cloudElem); //append
-    worldElem.append(cloudElem);
+    worldElem.appendChild(cloudElem);
     currentCloud += 1;
     console.log("cloud");
   }
